aa2b44098a9fb6e17b03afb94ad110484a449aa4
Douglas <douglasfabio.ifsp@gmail.com>
2023-03-31 12:36:08 -0300

    CRUD Api - OK

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        source: '/api/:Development*',
+        destination: 'http://localhost:5000/api/:Development*',
       },
     ]
   }
